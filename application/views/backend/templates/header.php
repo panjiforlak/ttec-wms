ad39d674f9d5e61b07c50c54c25aae2c3319f56b
panji <panjiforlak@gmail.com>
2024-08-12 02:40:41 +0700

    changes title

--- application/views/backend/templates/header.php
+++ application/views/backend/templates/header.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-    <title>PT.Tokai Extrusion</title>
+    <title>PT.TTEC</title>
     <meta content="" name="description">
     <meta content="" name="keywords">
 
